e5a0759d1329ea163e02e64302c0804e9d920976
nathan.sehn <nathan.sehn@zone46-ed.cs.ucalgary.ca>
2019-02-28 18:09:44 -0700

    Clean up indentation of file

--- src/DataStorage/Shelf.java
+++ src/DataStorage/Shelf.java
@@ -83,200 +83,200 @@ public class Shelf {
 		paperMedias.add(paper);
 	}
 	
-	/**
-	 * Media is loaded from file and put into shelf
-	 * TODO
-	 */
-	public void loadMedia() {
-		//load media from file.. so create objects of CDS,DVDS and Books/Magazines/Comics & store them in arrays of this class
+    /**
+     * Media is loaded from file and put into shelf
+     * TODO
+     */
+    public void loadMedia() {
+	//load media from file.. so create objects of CDS,DVDS and Books/Magazines/Comics & store them in arrays of this class
 		
-		//load CD from file
-		setCds(readCDFile(getCds()));
+	//load CD from file
+	setCds(readCDFile(getCds()));
 		
 		
-		//Load DVDs from file
-		setDvds(readDVDFile(getDvds()));
+	//Load DVDs from file
+	setDvds(readDVDFile(getDvds()));
 		 
-		//Load BOOKs from file
-		setPaperMedias(readPMFile(getPaperMedias()));
+	//Load BOOKs from file
+	setPaperMedias(readPMFile(getPaperMedias()));
 		 
 		
 		
-	}
+    }
 	
-	public String toString() {
-		String a = "";
-		String b = "";
-		String c = "";
+    public String toString() {
+	String a = "";
+	String b = "";
+	String c = "";
 		
-		int counter = 1;
+	int counter = 1;
 		
-		for (int i=0;i<cds.size();i++) {
-			a = counter + ". " + cds.get(i).toString() + "\n";
-		}
-		counter = 1;
-		for (int j=0;j<dvds.size();j++) {
-			b = counter+ ". " + dvds.get(j).toString() + "\n";
-		}
-		
-		counter = 1;
-		for (int k=0;k<paperMedias.size();k++) {
-			c = counter + ". "+ paperMedias.get(k).toString() + "\n";
-		}
-		
-		return "\n----------- On Virtual Shelf: ------------\n" + "\n------ CD's ------\n" +
-		a + "\n------ DVDS --------\n" + b + "\n------ PAPER MEDIA ------\n" +c + "\n------------------------------------------";
-				
+	for (int i=0;i<cds.size();i++) {
+	    a = counter + ". " + cds.get(i).toString() + "\n";
 	}
+	counter = 1;
+	for (int j=0;j<dvds.size();j++) {
+	    b = counter+ ". " + dvds.get(j).toString() + "\n";
+	}
+		
+	counter = 1;
+	for (int k=0;k<paperMedias.size();k++) {
+	    c = counter + ". "+ paperMedias.get(k).toString() + "\n";
+	}
+		
+	return "\n----------- On Virtual Shelf: ------------\n" + "\n------ CD's ------\n" +
+	    a + "\n------ DVDS --------\n" + b + "\n------ PAPER MEDIA ------\n" +c + "\n------------------------------------------";
+				
+    }
 
-		public ArrayList<CD> readCDFile(ArrayList<CD> loadedList) {
-			String fileName = "src/DataStorage/CDs.txt";
-			FileReader fr = null;
-			BufferedReader inputStream = null;
-			try {
-				fr = new FileReader(fileName);
-				inputStream = new BufferedReader(fr);
+    public ArrayList<CD> readCDFile(ArrayList<CD> loadedList) {
+	String fileName = "src/DataStorage/CDs.txt";
+	FileReader fr = null;
+	BufferedReader inputStream = null;
+	try {
+	    fr = new FileReader(fileName);
+	    inputStream = new BufferedReader(fr);
 				    
-				CD temp;
-				String line = null;
-				String loadedTitle = null;
-				String[] dates = null;
-				String status = null;
-				ArrayList<String> creators = new ArrayList<String>();
-				Calendar loadedDate = Calendar.getInstance();
+	    CD temp;
+	    String line = null;
+	    String loadedTitle = null;
+	    String[] dates = null;
+	    String status = null;
+	    ArrayList<String> creators = new ArrayList<String>();
+	    Calendar loadedDate = Calendar.getInstance();
 				    
-				    while((line = inputStream.readLine()) != null) {
-					if (line.length() <= 2) {continue;}
-					if (line.substring(0, 3).equals("END")) {
-					    temp = new CD(loadedTitle,creators, loadedDate,new Status(status));
-					    loadedList.add(temp);
-					    creators.clear();
-					    continue;
-					}
-					if (line.substring(0, 5).equals("Date:")) {
-					    dates = line.split(" ");
-					    loadedDate.set(Integer.valueOf(dates[dates.length-3]),Integer.valueOf(dates[dates.length-2]),Integer.valueOf(dates[dates.length-1]));
-					}
-					if (line.substring(0, 6).equals("Title:")) {
-					    loadedTitle = line.substring(7, line.length());
-					}
-					if (line.substring(0, 7).equals("Status:")) {
-					    status = line.substring(8, line.length());
-					}
-					if (line.substring(0, 8).equals("Creator:")) {
-					    creators.add(line.substring(9, line.length()));
-					}
-					else {continue;}
-				    }
-				}
-				catch(FileNotFoundException e) {
-				    System.out.println("Error opening file");
-				}
-				catch(IOException ioe) {
-				    System.out.println("Error reading file");
-				}
-				return loadedList;
-			    }
-	
-			    public ArrayList<DVD> readDVDFile(ArrayList<DVD> loadedList) {
-				String fileName = "src/DataStorage/DVDs.txt";
-				FileReader fr = null;
-				BufferedReader inputStream = null;
-				try {
-				    fr = new FileReader(fileName);
-				    inputStream = new BufferedReader(fr);
-				
-				    DVD temp;
-				    String line = null;
-				    String loadedTitle = null;
-				    String[] dates = null;
-				    String status = null;
-				    ArrayList<String> creators = new ArrayList<String>();
-				    Calendar loadedDate = Calendar.getInstance();
-	
-	
-				    while((line = inputStream.readLine()) != null) {
-					if (line.length() <= 2) {continue;}
-					if (line.substring(0, 3).equals("END")) {
-					    temp = new DVD(loadedTitle,creators, loadedDate,new Status(status));
-					    loadedList.add(temp);
-					    creators.clear();
-					    continue;
-					}
-					if (line.substring(0, 5).equals("Date:")) {
-					    dates = line.split(" ");
-					    loadedDate.set(Integer.valueOf(dates[dates.length-3]),Integer.valueOf(dates[dates.length-2]),Integer.valueOf(dates[dates.length-1]));
-					}
-					if (line.substring(0, 6).equals("Title:")) {
-					    loadedTitle = line.substring(7, line.length());
-					}
-					if (line.substring(0, 7).equals("Status:")) {
-					    status = line.substring(8, line.length());
-					}
-					if (line.substring(0, 8).equals("Creator:")) {
-					    creators.add(line.substring(9, line.length()));
-					}
-					else {continue;}
-				    }
-				}
-				catch(FileNotFoundException e) {
-				    System.out.println("Error opening file");
-				}
-				catch(IOException ioe) {
-				    System.out.println("Error reading file");
-				}
-				return loadedList;
-			    }
-	
-			    public ArrayList<PaperMedia> readPMFile(ArrayList<PaperMedia> loadedList) {
-				String fileName = "src/DataStorage/PaperMedia.txt";
-				FileReader fr = null;
-				BufferedReader inputStream = null;
-				try {
-				    fr = new FileReader(fileName);
-				    inputStream = new BufferedReader(fr);
-				
-				    PaperMedia temp;
-				    String line = null;
-				    String loadedTitle = null;
-				    String[] dates = null;
-				    String status = null;
-				    ArrayList<String> creators = new ArrayList<String>();
-				    Calendar loadedDate = Calendar.getInstance();
-	
-	
-				    while((line = inputStream.readLine()) != null) {
-					if (line.length() <= 2) {continue;}
-					if (line.substring(0, 3).equals("END")) {
-					    temp = new PaperMedia(loadedTitle,creators, loadedDate,new Status(status));
-					    loadedList.add(temp);
-					    creators.clear();
-					    continue;
-					}
-					if (line.substring(0, 5).equals("Date:")) {
-					    dates = line.split(" ");
-					    loadedDate.set(Integer.valueOf(dates[dates.length-3]),Integer.valueOf(dates[dates.length-2]),Integer.valueOf(dates[dates.length-1]));
-					}
-					if (line.substring(0, 6).equals("Title:")) {
-					    loadedTitle = line.substring(7, line.length());
-					}
-					if (line.substring(0, 7).equals("Status:")) {
-					    status = line.substring(8, line.length());
-					}
-					if (line.substring(0, 8).equals("Creator:")) {
-					    creators.add(line.substring(9, line.length()));
-					}
-					else {continue;}
-				    }
-				}
-				catch(FileNotFoundException e) {
-				    System.out.println("Error opening file");
-				}
-				catch(IOException ioe) {
-				    System.out.println("Error reading file");
-				}
-				return loadedList;
+	    while((line = inputStream.readLine()) != null) {
+		if (line.length() <= 2) {continue;}
+		if (line.substring(0, 3).equals("END")) {
+		    temp = new CD(loadedTitle,creators, loadedDate,new Status(status));
+		    loadedList.add(temp);
+		    creators.clear();
+		    continue;
+		}
+		if (line.substring(0, 5).equals("Date:")) {
+		    dates = line.split(" ");
+		    loadedDate.set(Integer.valueOf(dates[dates.length-3]),Integer.valueOf(dates[dates.length-2]),Integer.valueOf(dates[dates.length-1]));
+		}
+		if (line.substring(0, 6).equals("Title:")) {
+		    loadedTitle = line.substring(7, line.length());
+		}
+		if (line.substring(0, 7).equals("Status:")) {
+		    status = line.substring(8, line.length());
+		}
+		if (line.substring(0, 8).equals("Creator:")) {
+		    creators.add(line.substring(9, line.length()));
+		}
+		else {continue;}
+	    }
 	}
+	catch(FileNotFoundException e) {
+	    System.out.println("Error opening file");
+	}
+	catch(IOException ioe) {
+	    System.out.println("Error reading file");
+	}
+	return loadedList;
+    }
+	
+    public ArrayList<DVD> readDVDFile(ArrayList<DVD> loadedList) {
+	String fileName = "src/DataStorage/DVDs.txt";
+	FileReader fr = null;
+	BufferedReader inputStream = null;
+	try {
+	    fr = new FileReader(fileName);
+	    inputStream = new BufferedReader(fr);
+				
+	    DVD temp;
+	    String line = null;
+	    String loadedTitle = null;
+	    String[] dates = null;
+	    String status = null;
+	    ArrayList<String> creators = new ArrayList<String>();
+	    Calendar loadedDate = Calendar.getInstance();
+	
+	
+	    while((line = inputStream.readLine()) != null) {
+		if (line.length() <= 2) {continue;}
+		if (line.substring(0, 3).equals("END")) {
+		    temp = new DVD(loadedTitle,creators, loadedDate,new Status(status));
+		    loadedList.add(temp);
+		    creators.clear();
+		    continue;
+		}
+		if (line.substring(0, 5).equals("Date:")) {
+		    dates = line.split(" ");
+		    loadedDate.set(Integer.valueOf(dates[dates.length-3]),Integer.valueOf(dates[dates.length-2]),Integer.valueOf(dates[dates.length-1]));
+		}
+		if (line.substring(0, 6).equals("Title:")) {
+		    loadedTitle = line.substring(7, line.length());
+		}
+		if (line.substring(0, 7).equals("Status:")) {
+		    status = line.substring(8, line.length());
+		}
+		if (line.substring(0, 8).equals("Creator:")) {
+		    creators.add(line.substring(9, line.length()));
+		}
+		else {continue;}
+	    }
+	}
+	catch(FileNotFoundException e) {
+	    System.out.println("Error opening file");
+	}
+	catch(IOException ioe) {
+	    System.out.println("Error reading file");
+	}
+	return loadedList;
+    }
+	
+    public ArrayList<PaperMedia> readPMFile(ArrayList<PaperMedia> loadedList) {
+	String fileName = "src/DataStorage/PaperMedia.txt";
+	FileReader fr = null;
+	BufferedReader inputStream = null;
+	try {
+	    fr = new FileReader(fileName);
+	    inputStream = new BufferedReader(fr);
+				
+	    PaperMedia temp;
+	    String line = null;
+	    String loadedTitle = null;
+	    String[] dates = null;
+	    String status = null;
+	    ArrayList<String> creators = new ArrayList<String>();
+	    Calendar loadedDate = Calendar.getInstance();
+	
+	
+	    while((line = inputStream.readLine()) != null) {
+		if (line.length() <= 2) {continue;}
+		if (line.substring(0, 3).equals("END")) {
+		    temp = new PaperMedia(loadedTitle,creators, loadedDate,new Status(status));
+		    loadedList.add(temp);
+		    creators.clear();
+		    continue;
+		}
+		if (line.substring(0, 5).equals("Date:")) {
+		    dates = line.split(" ");
+		    loadedDate.set(Integer.valueOf(dates[dates.length-3]),Integer.valueOf(dates[dates.length-2]),Integer.valueOf(dates[dates.length-1]));
+		}
+		if (line.substring(0, 6).equals("Title:")) {
+		    loadedTitle = line.substring(7, line.length());
+		}
+		if (line.substring(0, 7).equals("Status:")) {
+		    status = line.substring(8, line.length());
+		}
+		if (line.substring(0, 8).equals("Creator:")) {
+		    creators.add(line.substring(9, line.length()));
+		}
+		else {continue;}
+	    }
+	}
+	catch(FileNotFoundException e) {
+	    System.out.println("Error opening file");
+	}
+	catch(IOException ioe) {
+	    System.out.println("Error reading file");
+	}
+	return loadedList;
+    }
 	
 }
 	
